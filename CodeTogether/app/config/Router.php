26c9cf283bc61bf019a4436c90bf9c43aca2b114
bdye01 <157627274+Dye21@users.noreply.github.com>
2025-10-13 21:20:14 -0500

    Base controller and router files

---
+++ CodeTogether/app/config/Router.php
@@ -0,0 +1,44 @@
+<?php
+    include_once "Controller.php";
+
+    class Router{
+        public $controllers;
+
+        public function __construct(){
+            $this->showErrors(0);
+            $this->controllers=[];
+        }
+
+        public function run(){
+            $action="default";
+
+            session_start();
+
+            if(isset($_REQUEST['action'])){
+                $action=$_REQUEST['action'];
+            }
+
+            $this->authcheck($action);
+                        
+            $controller = $this->controllers[$action];
+            $controller->performAction();
+        }
+
+        public function addController($action,$controller){
+            $this->controllers[$action] = $controller;
+        }
+
+        public function authCheck($action){
+            return;
+        }
+
+        public function showErrors($debug){
+            if($debug==1){
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL & ~E_NOTICE);
+            }
+        }
+    }
+
+?>
