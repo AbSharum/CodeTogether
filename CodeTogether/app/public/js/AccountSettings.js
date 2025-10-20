4824d9db61b1521cb8393ae7049987cf2301ac08
parkerfletcher1 <parker.fletcher1@gmail.com>
2025-10-19 20:38:30 -0500

    updated
    made changes to accountsettings

--- CodeTogether/app/public/js/AccountSettings.js
+++ CodeTogether/app/public/js/AccountSettings.js
@@ -38,7 +38,7 @@ const canvas = document.getElementById('matrix-canvas');
         }
     };
 
-    setInterval(draw, 105);
+    setInterval(draw, 35);
 
     window.addEventListener('resize', () => {
         canvas.width = window.innerWidth;
@@ -54,10 +54,36 @@ const canvas = document.getElementById('matrix-canvas');
     // Use requestAnimationFrame for a smoother animation loop
     function animate() {
         draw();
-        requestAnimationFrame(animate);
     }
 
     window.onload = function() {
         animate();
     };
+
+
+// Get the modal
+var modal = document.getElementById("deleteModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("delete");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
         
