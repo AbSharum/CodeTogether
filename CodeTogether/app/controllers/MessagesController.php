99bd7ee3ab8c85780ac64a2c2eafb5ff3ae58646
bdye01 <bdye01@uafs.edu>
2025-10-21 11:27:25 -0600

    updated navbar to work with index.php

---
+++ CodeTogether/app/controllers/MessagesController.php
@@ -0,0 +1,18 @@
+<?php
+    declare(strict_types=1);
+    include_once __DIR__ . "/../dao/MessageDAO.php";
+
+    class MessagesController extends Controller {
+        private MessageDAO $MessageDao;
+
+        public function performAction(): void {
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                $this->renderView('messages');
+            }
+        }
+
+        public function renderView(string $view, $messages = []): void {
+            include "./public/views/$view.php";
+        }
+    }
+?>
