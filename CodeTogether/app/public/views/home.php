09c0b470fdb265e227412a80423ff4ef5b526a0b
parkerfletcher1 <parker.fletcher1@gmail.com>
2025-10-24 00:42:41 -0500

    update
    i changed the navbar to the navbar php page

--- CodeTogether/app/public/views/home.php
+++ CodeTogether/app/public/views/home.php
@@ -19,49 +19,7 @@
 <body>
     <canvas id="matrix-canvas"></canvas>
     <!--NavBar-->
-    <nav class="navbar navbar-expand-lg navbar-dark sticky-top"
-        style="background-color: black; border-bottom: 2px solid green;">
-        <div class="container-fluid container-lg">
-            <a class="navbar-brand fw-bold text-info" href="/public/profile_page/profile.php">Code Together</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <form class="d-flex flex-grow-1 mx-3" role="search" action="index.php?action=search" method="get">
-                <input class="form-control form-control-sm me-2 bg-dark text-white border-success"
-                    type="search" name="q" placeholder="Search users or posts..." aria-label="Search">
-                <button class="btn btn-outline-success btn-sm" type="submit">
-                    <i class="fas fa-search"></i>
-                </button>
-            </form>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item" id="home-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=home"><i class="fas fa-home me-1"></i>
-                            Home</a> <!--need to verify correct paths -->
-                    </li>
-                    <li class="nav-item" id="game-nav-item">
-                        <a class="nav-link text-white" href="/public/views/game.html"><i
-                                class="fas fa-gamepad me-1"></i> Game Page</a> <!--need to verify correct paths -->
-                    </li>
-                    <li class="nav-item" id="message-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=messages"><i
-                                class="fas fa-envelope me-1"></i> Messages</a> <!--need to verify correct paths -->
-                    </li>
-                    <li class="nav-item" id="accountSettings-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=accountSettings"><i
-                                class="fas fa-cog me-1"></i> Account Settings</a> <!--need to verify correct paths -->
-                    </li>
-                    <li class="nav-item" id="login-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=login"><i class="fa fa-sign-in" style="color: blue"></i> Login</a> <!--need to actually log user out-->
-                    </li>
-                    <li class="nav-item" id="logout-nav-item">
-                        <a class="nav-link text-white" href="#"><i class="fa fa-sign-out" style="color: red"></i>Logout</a> <!--need to actually log user out-->
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include 'navbar.php'; ?>
     <!--end of navigation-->
 
     <div class="container py-5">
