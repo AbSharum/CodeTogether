dbbe4c99ccf2df9eea5fbede1dc68740ca31f3c3
parkerfletcher1 <parker.fletcher1@gmail.com>
2025-10-26 18:35:24 -0500

    updated

--- CodeTogether/app/public/views/game.php
+++ CodeTogether/app/public/views/game.php
@@ -13,59 +13,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         xintegrity="sha512-SnH5WK+bZxgPHs44uWIX+LLMDJc5nI6Jj4QkI7U1vKjK+L0n4A0w4Z+T5E5R5B5B5Y5S5T5W5V5U5T5Q5V5W5X5Y5Z5"
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="game.css">
+    <link rel="stylesheet" href="/public/css/game.css">
 </head>
 
 <body>
     <canvas id="matrix-canvas"></canvas>
     <!--NavBar-->
-    <nav class="navbar navbar-expand-lg navbar-dark sticky-top"
-        style="background-color: black; border-bottom: 2px solid green;">
-        <div class="container-fluid container-lg">
-            <a class="navbar-brand fw-bold brand-green text-info" href="index.php?action=landing">Code Together</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                <form class="d-flex w-50 w-lg-auto me-lg-3" style="max-width: 200px;" role="search" action="index.php?action=search" method="POST">
-                    <input class="form-control me-2 w-100"
-                        type="search"
-                        name="search"
-                        placeholder="Search..."
-                        aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </form>
-                    
-                    <li class="nav-item" id="home-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=home"><i class="fas fa-home me-1"></i>
-                            Home</a> <!--need to verify correct paths -->
-                    </li>
-                    <li class="nav-item" id="game-nav-item">
-                        <a class="nav-link text-white" href="/public/views/game.html"><i
-                                class="fas fa-gamepad me-1"></i> Game Page</a> <!--need to verify correct paths -->
-                    </li>
-                    <li class="nav-item" id="message-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=messages"><i
-                                class="fas fa-envelope me-1"></i> Messages</a> <!--need to verify correct paths -->
-                    </li>
-                    <li class="nav-item" id="accountSettings-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=accountSettings"><i
-                                class="fas fa-cog me-1"></i> Account Settings</a> <!--need to verify correct paths -->
-                    </li>
-                    <li class="nav-item" id="login-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=login"><i class="fa fa-sign-in" style="color: blue"></i> Login</a> <!--need to actually log user out-->
-                    </li>
-                    <li class="nav-item" id="logout-nav-item">
-                        <a class="nav-link text-white" href="index.php?action=logout"><i class="fa fa-sign-out" style="color: red"></i>Logout</a> <!--need to actually log user out-->
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include __DIR__ .'/../includes/navbar.php'; ?>
     <!--end of navigation-->
 
 <div class="container-fluid container-lg py-5">
@@ -190,6 +144,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
         xintegrity="sha384-0pUGZvbkm6XF6gxjEnlwpMCEoV3f73SjJ+J8C6W6D2Kx5lM7B8K2FfR7R7E7Q"
         crossorigin="anonymous"></script>
-    <script src="games.js"></script>
+    <script src="/public/js/games.js"></script>
 </body>
 </html>
